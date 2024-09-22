7087cbb86cb1654091ffe2e9018e207de36e87bf
antarikshm <antarikshmeshram@gmail.com>
2024-09-23 00:06:27 +0530

    Job Listing project

---
+++ src/main/java/com/org/jobs/joblistings/JoblistingsApplication.java
@@ -0,0 +1,13 @@
+package com.org.jobs.joblistings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblistingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblistingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/jobs/joblistings/JoblistingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.jobs.joblistings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
